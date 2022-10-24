df157c6cdd69792caffcfdeb405e4f288a7186d1
mlhiter <3076438032@qq.com>
2022-10-24 18:34:31 +0800

    ignore文件

--- src/libs/database/categoryTable.py
+++ src/libs/database/categoryTable.py
@@ -11,13 +11,13 @@ filePath = "../../../data/data.json"
 with open(filePath, mode="r", encoding="utf-8") as f:
     dataJSON = f.read()  # 字符串
 data = json.loads(dataJSON)  # 获得分类数据
-deal_data = {}
+deal_data = []
 for item in data:
     deal_item = {"type_id": item["type_id"], "type_name": item["name"]}
     story_list = []
     for story in item["story"]:
         story_list.append(story["name"])
-    deal_item.append(story_list)
+    deal_item["story_list"] = story_list
     deal_data.append(deal_item)
 print(deal_data)
 print("全部分类信息已存入category-table！")
